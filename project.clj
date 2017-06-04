e8fd3c77509a552497f9a0d23bb57b1a85f495ba
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-06-04 14:56:34 +0530

    update deps - 0.2.0 GA version of other bract modules, lein-ring 0.12.0

--- project.clj
+++ project.clj
@@ -9,18 +9,18 @@
   :min-lein-version "2.7.1"
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [bract/bract.cli  "0.2.0-SNAPSHOT"]
-                 [bract/bract.ring "0.2.0-SNAPSHOT"]
+                 [bract/bract.cli  "0.2.0"]
+                 [bract/bract.ring "0.2.0"]
                  ;; web
                  [ring/ring-core "1.5.1"]
                  [http-kit "2.2.0"]]
   :target-path "target/%s"
-  :plugins [[lein-ring "0.11.0"]]
+  :plugins [[lein-ring "0.12.0"]]
   :ring {:handler bract.ring.dev/handler
          :init    bract.ring.dev/init!
          :port    3000
          :nrepl   {:start? true :port 3001}}
-  :profiles {:dev {:dependencies [[bract/bract.dev "0.2.0-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[bract/bract.dev "0.2.0"]]
                    :source-paths ["dev"]}
              :uberjar {:aot [bract.cli.main]
                        :main ^:skip-aot bract.cli.main}})

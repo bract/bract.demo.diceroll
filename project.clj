04ea605eaf7edcea38ba6e0f93ee12c1183808c3
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-03-07 18:53:40 +0530

    kick off 0.6.0 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bract/demo.diceroll "0.5.1"
+(defproject bract/demo.diceroll "0.6.0-SNAPSHOT"
   :description "Bract sample Ring webapp to roll a dice"
   :url "https://github.com/bract/demo.diceroll"
   :license {:name "Eclipse Public License"

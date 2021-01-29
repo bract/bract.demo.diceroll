a10353ac2274a3badc4a1ceed6e9c8821f726455
Shantanu Kumar <kumar.shantanu@gmail.com>
2021-01-29 12:40:01 +0530

    use Bract 0.6.2

--- project.clj
+++ project.clj
@@ -8,9 +8,9 @@
                 *unchecked-math* :warn-on-boxed}
   :min-lein-version "2.7.1"
   :pedantic?    :warn
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [bract/bract.cli     "0.6.1-0.1.0"]
-                 [bract/bract.ring    "0.6.1-0.1.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [bract/bract.cli     "0.6.2-0.1.1-beta2"]
+                 [bract/bract.ring    "0.6.2-0.2.0-beta2"]
                  ;; web servers (uncomment any one)
                  ;;[aleph                   "0.4.6" :exclusions [org.clojure/tools.logging]]
                  [http-kit                "2.3.0"]
@@ -19,13 +19,17 @@
                  ;; web
                  [ring/ring-core "1.6.3"]]
   :target-path "target/%s"
-  :plugins [[lein-ring "0.12.3"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler bract.ring.dev/handler
          :init    bract.ring.dev/init!
          :port    3000
          :nrepl   {:start? true :port 3001}}
-  :profiles {:dev {:dependencies [[bract/bract.dev "0.6.1-0.1.0"]]
-                   :source-paths ["dev"]}
+  :profiles {:dev {:dependencies [[bract/bract.dev "0.6.2-0.2.0-beta2" :exclusions [bract/bract.core]]]
+                   :repl-options {:init-ns bract.dev.repl
+                                  :port 3001}
+                   ;; :source-paths ["dev"]
+                   :main ^:skip-aot bract.core.main
+                   }
              :uberjar {:aot [bract.core.main]
                        :main ^:skip-aot bract.core.main
                        :pedantic? :abort}})
